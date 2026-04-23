6f3ac2613c5953c9a31f3aa5e8cf0d2627355d6b
José Dotta <josehenriquedotta61@gmail.com>
2026-04-23 11:10:28 -0300

    🔨 Adicionando arquivos para facilitar uso da CLI dotnet

---
+++ dev.ps1
@@ -0,0 +1,31 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("update-tool", "run", "watch", "db-update", "mig-add")]
+    $Action,
+
+    [Parameter(Mandatory=$false)]
+    $Name = "InitialMigration"
+)
+
+switch ($Action) {
+    "update-tool" {
+        Write-Host "--- Atualizando dotnet-ef globalmente ---" -ForegroundColor Cyan
+        dotnet tool update --global dotnet-ef
+    }
+    "run" {
+        Write-Host "--- Iniciando API AutoReparos ---" -ForegroundColor Green
+        dotnet run --project .\AutoReparos.API\
+    }
+    "watch" {
+        Write-Host "--- Iniciando Hot Reload (Watch) ---" -ForegroundColor Yellow
+        dotnet watch --project .\AutoReparos.API\
+    }
+    "db-update" {
+        Write-Host "--- Aplicando Migrations no Banco de Dados ---" -ForegroundColor Blue
+        dotnet ef database update --project .\AutoReparos.Infra\ --startup-project .\AutoReparos.API\
+    }
+    "mig-add" {
+        Write-Host "--- Criando nova Migration: $Name ---" -ForegroundColor Magenta
+        dotnet ef migrations add $Name --project .\AutoReparos.Infra\ --startup-project .\AutoReparos.API\
+    }
+}
