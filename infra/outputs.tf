afca3b7d130494469580d54fcb364220ffba101f
José Dotta <josehenriquedotta61@gmail.com>
2026-07-12 04:56:02 -0300

    :wrench: Ajustando configurações do Terraform para compatibilidade com Helm provider v3 e ignorando diretório .terraform

---
+++ infra/outputs.tf
@@ -0,0 +1,34 @@
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "The ID of the VPC"
+}
+
+output "private_subnet_ids" {
+  value       = module.vpc.private_subnet_ids
+  description = "IDs of the private subnets"
+}
+
+output "public_subnet_ids" {
+  value       = module.vpc.public_subnet_ids
+  description = "IDs of the public subnets"
+}
+
+output "cluster_name" {
+  value       = module.eks.cluster_name
+  description = "The Name of the EKS Cluster"
+}
+
+output "cluster_endpoint" {
+  value       = module.eks.cluster_endpoint
+  description = "The endpoint for your EKS Kubernetes API"
+}
+
+output "oidc_provider_url" {
+  value       = module.eks.oidc_provider_url
+  description = "OIDC Provider URL for IAM roles for service accounts (IRSA)"
+}
+
+output "ebs_csi_addon_arn" {
+  value       = module.addons.ebs_csi_addon_arn
+  description = "ARN of the EBS CSI driver addon"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,65 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS deployment region"
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the EKS cluster"
+  default     = "autoreparos-cluster"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment name"
+  default     = "production"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for public subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks for private subnets"
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "Availability zones for the subnets"
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "instance_types" {
+  type        = list(string)
+  description = "Worker nodes EC2 instance types"
+  default     = ["t3.medium"]
+}
+
+variable "desired_size" {
+  type        = number
+  description = "Desired number of worker nodes"
+  default     = 2
+}
+
+variable "max_size" {
+  type        = number
+  description = "Maximum number of worker nodes"
+  default     = 2
+}
+
+variable "min_size" {
+  type        = number
+  description = "Minimum number of worker nodes"
+  default     = 2
+}
