a0239417f2c3836f53a7b51b746e6bbf30121f05
José Dotta <josehenriquedotta61@gmail.com>
2026-07-13 21:59:13 -0300

    :rotating_light: Corrigindo injeção de segredos no workflow e ifs no bash script

--- infra/update-kubeconfig.sh
+++ infra/update-kubeconfig.sh
@@ -8,16 +8,16 @@ DEFAULT_CLUSTER="autoreparos-cluster"
 INFRA_DIR="$(dirname "$0")"
 VARIABLES_FILE="$INFRA_DIR/variables.tf"
 
-if [ -f "$VARIABLES_FILE" ]; then
+if [[ -f "$VARIABLES_FILE" ]]; then
   # Tenta extrair a região padrão do variables.tf
   EXTRACTED_REGION=$(grep -A 3 'variable "aws_region"' "$VARIABLES_FILE" | grep 'default' | cut -d'"' -f2)
-  if [ ! -z "$EXTRACTED_REGION" ]; then
+  if [[ ! -z "$EXTRACTED_REGION" ]]; then
     DEFAULT_REGION="$EXTRACTED_REGION"
   fi
 
   # Tenta extrair o nome do cluster padrão do variables.tf
   EXTRACTED_CLUSTER=$(grep -A 3 'variable "cluster_name"' "$VARIABLES_FILE" | grep 'default' | cut -d'"' -f2)
-  if [ ! -z "$EXTRACTED_CLUSTER" ]; then
+  if [[ ! -z "$EXTRACTED_CLUSTER" ]]; then
     DEFAULT_CLUSTER="$EXTRACTED_CLUSTER"
   fi
 fi
@@ -31,7 +31,7 @@ echo "Cluster: $CLUSTER_NAME"
 
 aws eks update-kubeconfig --region "$REGION" --name "$CLUSTER_NAME"
 
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   echo "Sucesso! Testando conexão com o cluster..."
   kubectl get svc
 else
