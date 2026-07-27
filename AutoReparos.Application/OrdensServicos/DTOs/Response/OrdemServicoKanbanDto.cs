4792538ab63970e9ff2ba8410147dd85be1f0924
José Dotta <josehenriquedotta61@gmail.com>
2026-07-27 14:58:35 -0300

    🎨 Removendo TempoDiagnostico alucinado e usando dados reais no Kanban

--- AutoReparos.Application/OrdensServicos/DTOs/Response/OrdemServicoKanbanDto.cs
+++ AutoReparos.Application/OrdensServicos/DTOs/Response/OrdemServicoKanbanDto.cs
@@ -1,18 +1,68 @@
 using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace AutoReparos.Application.OrdensServicos.DTOs.Response
 {
-    public record OrdemServicoKanbanDto(
-        Guid Id,
-        string NumeroOS,
-        string ClienteNome,
-        string PlacaVeiculo,
-        string ModeloVeiculo,
-        string Status,
-        decimal ValorTotal,
-        IEnumerable<ItemServicoKanbanDto> ItensServico
-    );
+    public record KanbanColumnDto(string Status, IEnumerable<KanbanCardDto> Cards);
 
-    public record ItemServicoKanbanDto(Guid Id, bool Concluido);
+    [JsonDerivedType(typeof(ReceivedKanbanCardDto), "Received")]
+    [JsonDerivedType(typeof(DiagnosisKanbanCardDto), "Diagnosis")]
+    [JsonDerivedType(typeof(ApprovalKanbanCardDto), "Approval")]
+    [JsonDerivedType(typeof(ExecutionKanbanCardDto), "Execution")]
+    [JsonDerivedType(typeof(FinishedKanbanCardDto), "Finished")]
+    public abstract record KanbanCardDto;
+
+    public record ReceivedKanbanCardDto(
+        Guid Id, 
+        string NumeroOS, 
+        string ClienteNome, 
+        string PlacaVeiculo, 
+        string ModeloVeiculo, 
+        string Status, 
+        DateTime DataEntrada) : KanbanCardDto;
+
+    public record DiagnosisKanbanCardDto(
+        Guid Id, 
+        string NumeroOS, 
+        string ClienteNome, 
+        string PlacaVeiculo, 
+        string ModeloVeiculo, 
+        string Status, 
+        string? ResponsavelId) : KanbanCardDto;
+
+    public record ApprovalKanbanCardDto(
+        Guid Id, 
+        string NumeroOS, 
+        string ClienteNome, 
+        string PlacaVeiculo, 
+        string ModeloVeiculo, 
+        string Status, 
+        decimal ValorOrcamento, 
+        int QuantidadeServicos, 
+        DateTime? EnvioAprovacaoEm) : KanbanCardDto;
+
+    public record ExecutionKanbanCardDto(
+        Guid Id, 
+        string NumeroOS, 
+        string ClienteNome, 
+        string PlacaVeiculo, 
+        string ModeloVeiculo, 
+        string Status, 
+        decimal ValorAprovado, 
+        double ProgressoServicos, 
+        string? MecanicoResponsavel,
+        int ServicosConcluidos,
+        int ServicosTotal) : KanbanCardDto;
+
+    public record FinishedKanbanCardDto(
+        Guid Id, 
+        string NumeroOS, 
+        string ClienteNome, 
+        string PlacaVeiculo, 
+        string ModeloVeiculo, 
+        string Status, 
+        decimal ValorFinal, 
+        DateTime DataConclusao, 
+        string StatusEntrega) : KanbanCardDto;
 }
--- AutoReparos.Application/OrdensServicos/UseCases/Core/ListarKanbanOrdensServicoUseCase.cs
+++ AutoReparos.Application/OrdensServicos/UseCases/Core/ListarKanbanOrdensServicoUseCase.cs
@@ -1,9 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoReparos.Application.OrdensServicos.DTOs.Response;
 using AutoReparos.Application.OrdensServicos.UseCases.Core.Interfaces;
-using AutoReparos.Application.Shared;
 using AutoReparos.Domain.Clientes.Repositories;
 using AutoReparos.Domain.OrdensServicos.Repositories;
 using AutoReparos.Domain.Veiculos.Repositories;
@@ -15,36 +15,101 @@ namespace AutoReparos.Application.OrdensServicos.UseCases.Core
         IClienteRepository clienteRepository,
         IVeiculoRepository veiculoRepository) : IListarKanbanOrdensServicoUseCase
     {
-        public async Task<PagedResult<OrdemServicoKanbanDto>> ExecuteAsync(PagedRequest request)
+        public async Task<IEnumerable<KanbanColumnDto>> ExecuteAsync()
         {
-            var (items, total) = await ordemServicoRepository.GetKanban(request.Skip, request.PageSize);
+            var (items, _) = await ordemServicoRepository.GetKanban(0, 1000);
             
-            var dtos = new List<OrdemServicoKanbanDto>();
+            var clienteIds = items.Select(x => x.ClienteId).Distinct();
+            var veiculoIds = items.Select(x => x.VeiculoId).Distinct();
+
+            var clientes = (await clienteRepository.GetByIds(clienteIds)).ToDictionary(x => x.Id);
+            var veiculos = (await veiculoRepository.GetByIds(veiculoIds)).ToDictionary(x => x.Id);
+
+            var columns = new Dictionary<string, List<KanbanCardDto>>
+            {
+                { "Recebida", new List<KanbanCardDto>() },
+                { "Diagnostico", new List<KanbanCardDto>() },
+                { "Aprovacao", new List<KanbanCardDto>() },
+                { "Execucao", new List<KanbanCardDto>() },
+                { "Finalizada", new List<KanbanCardDto>() }
+            };
+
             foreach (var os in items)
             {
-                var cliente = await clienteRepository.GetById(os.ClienteId);
-                var veiculo = await veiculoRepository.GetById(os.VeiculoId);
+                if ((os.Status == AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Finalizada || 
+                     os.Status == AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Entregue) && 
+                    (os.FinalizadoEm == null || os.FinalizadoEm.Value.ToLocalTime().Date != DateTime.Today))
+                {
+                    continue;
+                }
+
+                var cliente = clientes.GetValueOrDefault(os.ClienteId);
+                var veiculo = veiculos.GetValueOrDefault(os.VeiculoId);
                 
                 var clienteNome = cliente?.Nome ?? "Cliente não encontrado";
                 var placaVeiculo = veiculo?.Placa?.Valor ?? "Placa não encontrada";
                 var modeloVeiculo = veiculo?.Modelo ?? "Modelo não encontrado";
                 var numeroOS = os.Id.ToString()[..8].ToUpper();
                 
-                var itensServico = os.Servicos.Select(s => new ItemServicoKanbanDto(s.Id, s.Status == AutoReparos.Domain.OrdensServicos.Enums.EStatusServicoOS.Concluido));
-                
-                dtos.Add(new OrdemServicoKanbanDto(
-                    os.Id,
-                    numeroOS,
-                    clienteNome,
-                    placaVeiculo,
-                    modeloVeiculo,
-                    os.Status.ToString(),
-                    os.ValorTotal,
-                    itensServico
-                ));
+                KanbanCardDto card = os.Status switch
+                {
+                    AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Recebida =>
+                        new ReceivedKanbanCardDto(
+                            os.Id, numeroOS, clienteNome, placaVeiculo, modeloVeiculo, 
+                            os.Status.ToString(), os.CriadoEm),
+                            
+                    AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.EmDiagnostico =>
+                        new DiagnosisKanbanCardDto(
+                            os.Id, numeroOS, clienteNome, placaVeiculo, modeloVeiculo, 
+                            os.Status.ToString(), os.ResponsavelId),
+                            
+                    AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.AguardandoAprovacao =>
+                        new ApprovalKanbanCardDto(
+                            os.Id, numeroOS, clienteNome, placaVeiculo, modeloVeiculo, 
+                            os.Status.ToString(), os.ValorTotal, os.Servicos.Count, os.EnvioAprovacaoEm),
+                            
+                    AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.EmExecucao =>
+                        new ExecutionKanbanCardDto(
+                            os.Id, numeroOS, clienteNome, placaVeiculo, modeloVeiculo, 
+                            os.Status.ToString(), os.ValorTotal, 
+                            os.Servicos.Count > 0 
+                                ? (double)os.Servicos.Count(s => s.Status == AutoReparos.Domain.OrdensServicos.Enums.EStatusServicoOS.Concluido) / os.Servicos.Count * 100 
+                                : 0, 
+                            "Mecânico Chefe",
+                            os.Servicos.Count(s => s.Status == AutoReparos.Domain.OrdensServicos.Enums.EStatusServicoOS.Concluido),
+                            os.Servicos.Count),
+                            
+                    AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Finalizada or 
+                    AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Entregue =>
+                        new FinishedKanbanCardDto(
+                            os.Id, numeroOS, clienteNome, placaVeiculo, modeloVeiculo, 
+                            os.Status.ToString(), os.ValorTotal, os.FinalizadoEm ?? DateTime.UtcNow, 
+                            os.Status == AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Entregue ? "Entregue" : "Aguardando Entrega"),
+                            
+                    _ => null!
+                };
+
+                if (card != null)
+                {
+                    string? column = os.Status switch
+                    {
+                        AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Recebida => "Recebida",
+                        AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.EmDiagnostico => "Diagnostico",
+                        AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.AguardandoAprovacao => "Aprovacao",
+                        AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.EmExecucao => "Execucao",
+                        AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Finalizada or 
+                        AutoReparos.Domain.OrdensServicos.Enums.EStatusOrdemServico.Entregue => "Finalizada",
+                        _ => null
+                    };
+
+                    if (column != null)
+                    {
+                        columns[column].Add(card);
+                    }
+                }
             }
             
-            return new PagedResult<OrdemServicoKanbanDto>(dtos, total, request.PageNumber, request.PageSize);
+            return columns.Select(kvp => new KanbanColumnDto(kvp.Key, kvp.Value));
         }
     }
 }
